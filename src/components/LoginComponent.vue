adbcc0a2270203827477bcc439bb700bd93341d9
José Rocha <joserocha@MacBook-Air-de-Jose.local>
2023-11-19 11:40:04 -0300

    Limpado os campos de email e senha

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -10,8 +10,8 @@ const store = useDataStore();
 const router = useRouter();
 const $q = useQuasar();
 
-const email = ref('josemarlerocha@gmail.com');
-const senha = ref('123456');
+const email = ref('');
+const senha = ref('');
 
 const emailRegex = new RegExp('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:'
   + '[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0'
